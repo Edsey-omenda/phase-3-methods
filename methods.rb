ce67e6a7261b00cc6789b998c8e223970f9d9413
Edsey omenda <omendajames0317@gmail.com>
2022-08-16 17:15:30 +0300

    completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def  greet(name)
+
+    puts "Hello, #{name}!"
+end
+
+greet("Wuod Nyagoro")
+
+def greet_with_default(name = "programmer")
+
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("James")
+
+def add(num1, num2)
+    # puts num1 + num2
+    return num1 + num2
+end
+
+add(7,8)
+
+def halve(number)
+
+    if number.class != Integer
+        return nil
+    end
+        number / 2
+end
+halve("18")
